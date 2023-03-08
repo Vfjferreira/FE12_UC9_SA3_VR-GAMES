2586959b3cb4bd2ace7d99edce3f9e688de8ece7
Vfjferreira <vfjferreira@gmail.com>
2023-03-08 20:52:22 -0300

    angular_01

--- VR-GAMES/src/app/app-routing.module.ts
+++ VR-GAMES/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+const routes: Routes = [
+  {path:"header", component: HeaderComponent},
+  {path:"footer", component: FooterComponent}
+];
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
